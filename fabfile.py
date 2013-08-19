ac2d2c36643f60b617e05db90cfe7b3fba48936f
hudvin <hudvin@localhost.localdomain>
2013-08-19 23:53:12 +0300

    some changes

--- fabfile.py
+++ fabfile.py
@@ -39,7 +39,7 @@ def hello():
     sudo('rm /etc/my.cnf')
     files.append(my_cnf_file, my_cnf_body, use_sudo=True)
     sudo('service mysqld restart')
-    #sudo('mysql_secure_installation')
+    sudo('mysql_secure_installation')
     files.comment('/etc/selinux/config','SELINUX=enforcing',use_sudo=True)
     files.append('/etc/selinux/config','SELINUX=permissive', use_sudo=True)
     sudo("setenforce permissive")
@@ -64,17 +64,17 @@ def setup_nfs():
     files.append('/etc/exports','/export  *(rw,async,no_root_squash)', use_sudo=True)
     sudo('exportfs -a')
     files.append('/etc/sysconfig/nfs', nfs_sysconfig, use_sudo=True)
-    sudo('service iptables stop')
+    #sudo('service iptables stop')
     files.append('/etc/idmapd.conf', 'Domain=localhost.localdomain', use_sudo=True)
     sudo('service rpcbind start')
     sudo('service nfs start')
-    sudo('mkdir -p /mnt/primary')
-    sudo('mkdir -p /mnt/secondary')
-    sudo('mount -t nfs 127.0.0.1:/export/primary /mnt/primary')
-    sudo('mount -t nfs 127.0.0.1:/export/secondary /mnt/secondary') 
+#    sudo('mkdir -p /mnt/primary')
+    #sudo('mkdir -p /mnt/secondary')
+#   # sudo('mount -t nfs 127.0.0.1:/export/primary /mnt/primary')
+#    sudo('mount -t nfs 127.0.0.1:/export/secondary /mnt/secondary') 
 
 def download_system_vm():
-    sudo('/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt -m /mnt/secondary -u http://download.cloud.com/templates/acton/acton-systemvm-02062012.qcow2.bz2 -h kvm  -F')
+    sudo('/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt -m /export/secondary -u http://download.cloud.com/templates/acton/acton-systemvm-02062012.qcow2.bz2 -h kvm  -F')
 
 libvirtd_conf_body = """
 listen_tls = 0
@@ -100,7 +100,7 @@ HOTPLUG=no
 BOOTPROTO=none
 TYPE=Ethernet
 """
-eth0.100_body="""
+eth0_100_body="""
 DEVICE=eth0.100
 HWADDR=F0:DE:F1:A0:0E:D4
 ONBOOT=yes
@@ -112,7 +112,7 @@ IPADDR=192.168.0.120
 GATEWAY=192.168.0.1
 NETMASK=255.255.255.0
 """
-eth0.200_body="""
+eth0_200_body="""
 DEVICE=eth0.200
 HWADDR=F0:DE:F1:A0:0E:D4
 ONBOOT=yes
@@ -123,7 +123,7 @@ VLAN=yes
 BRIDGE=cloudbr0
 """
 
-eth0.300_body="""
+eth0_300_body="""
 DEVICE=eth0.300
 HWADDR=F0:DE:F1:A0:0E:D4
 ONBOOT=yes
@@ -145,7 +145,7 @@ DELAY=5
 STP=yes
 """
 
-cloudbr0_body="""
+cloudbr1_body="""
 DEVICE=cloudbr1
 TYPE=Bridge
 ONBOOT=yes
@@ -159,19 +159,22 @@ STP=yes
 
 def setup_eth():
     eth0_path = '/etc/sysconfig/network-scripts/ifcfg-eth0'
-    eth0.100_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.100'
-    eth0.200_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.200'
-    eth0.300_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.300'
+    eth0_100_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.100'
+    eth0_200_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.200'
+    eth0_300_path = '/etc/sysconfig/network-scripts/ifcfg-eth0.300'
     cloudbr0_path = '/etc/sysconfig/network-scripts/ifcfg-cloudbr0' 
     cloudbr1_path = '/etc/sysconfig/network-scripts/ifcfg-cloudbr1'
     #append
     files.append(eth0_path, eth0_body, use_sudo=True)
-    files.append(eth0.100_path, eth0.100_body, use_sudo=True)
-    files.append(eth0.200_path, eth0.200_body, use_sudo=True)
-    files.append(eth0.300_path, eth0.300_body, use_sudo=True)
+    files.append(eth0_100_path, eth0_100_body, use_sudo=True)
+    files.append(eth0_200_path, eth0_200_body, use_sudo=True)
+    files.append(eth0_300_path, eth0_300_body, use_sudo=True)
     files.append(cloudbr0_path, cloudbr0_body, use_sudo=True)
     files.append(cloudbr1_path, cloudbr1_body, use_sudo=True)
     sudo('rm /etc/udev/rules.d/70-persistent-net.rules')
     sudo('reboot')
     
 
+
+def setup_iptables():
+    pass	
